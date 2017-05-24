9d5b202811266fbdc538826f077487148b23d88b
Josef <soslan29@gmail.com>
2017-05-24 16:12:40 +0300

    Added function which upload file to firebase storage

---
+++ TabBar/StorageManager.swift
@@ -0,0 +1,26 @@
+//
+//  StorageManager.swift
+//  TabBar
+//
+//  Created by Иосиф Дзеранов on 24/05/2017.
+//  Copyright © 2017 IO Dzeranov. All rights reserved.
+//
+
+import Foundation
+import  Firebase
+
+class StorageManager{
+    
+    let reference = Storage.storage().reference(withPath: "images")
+    
+    func UploadFile(data:Data, name:String) {
+        let ref = reference.child(name)
+        ref.putData(data, metadata: nil) { (storage, error) in
+            if let error = error {
+                debugPrint(error)
+            }
+            debugPrint("success upload file")
+        }
+    }
+    
+}
